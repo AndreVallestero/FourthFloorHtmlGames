60cf92990372547c88773d9e4a9f868db181d166
Andre Vallestero <andrevallestero@gmail.com>
2018-11-14 14:18:30 -0500

    Implemented basic physics and completed rendering

--- LastMansStand/app.js
+++ LastMansStand/app.js
@@ -11,12 +11,14 @@
 */
 
 const INV_ROOT_2 = 0.70710678118,
-	GAME_DISP_RATIO = 534.0/766.0;
+	GAME_DISP_HEIGHT = 534.0,
+	GAME_DISP_WIDTH = 766.0,
+	GAME_DISP_RATIO = GAME_DISP_HEIGHT/GAME_DISP_WIDTH;
 
 var canvas, context, spritesheet, tpl, prevFrameTime, currFrameTime,
 	dispScale = 1,
 	gameState = 0, // 0 = Start menu, 1 = In game, 2 = Game over
-	invRatio = 383, // Game width / 2
+	invRatio = GAME_DISP_WIDTH / 2.0, 
 	keyStates = new Array(256).fill(0),
 	entities = {
 		"Player":[],
@@ -43,14 +45,14 @@ var canvas, context, spritesheet, tpl, prevFrameTime, currFrameTime,
 function main() {
 	canvas = document.getElementById('canvas');
 	context = canvas.getContext('2d');
+
 	spritesheet = new Image();
+	// Draw game start menu
+	spritesheet.onload = function() {draw_sprite(spritemap["main"][0]);};
 	spritesheet.src = "res/spritesheet.png";
 
 	resize();
 	add_event_listeners();
-
-	// Draw game start menu
-	draw_sprite(spritemap["main"][0]);
 
 	console.log("Application successfully initialized");
 }
@@ -59,8 +61,8 @@ function add_event_listeners() {
 	window.addEventListener('resize', resize);
 	window.onfocus = function() {};
 	window.onblur = function() {};
-	document.addEventListener('keydown', function(event){keyStates[event.keyCode]=1;});
-	document.addEventListener('keyup', function(event){keyStates[event.keyCodes]=0;});
+	document.addEventListener('keydown',function(event){keyStates[event.keyCode]=1;});
+	document.addEventListener('keyup',function(event){keyStates[event.keyCode]=0;});
 	document.addEventListener('keypress', key_press);
 	canvas.addEventListener('click', click);
 }
@@ -76,10 +78,18 @@ function resize() {
 
 	invRatio = canvas.height/ (GAME_DISP_RATIO+ GAME_DISP_RATIO);
 	dispScale = canvas.width/766.0;
+
+	if (gameState == 0) {
+		draw_sprite(spritemap["main"][0]);
+	} else if (gameState == 2) {
+		draw_sprite(spritemap["main"][1]);
+	}
 }
 
 function key_press(event) {
-	if(gameState == 0 || gameState == 2 || event.keyCode == 82) {
+	// Start game loop
+	if((gameState == 0 || gameState == 2) && (event.keyCode == 82 || event.which == 114)) {
+		entities["Player"].push(new Player());
 		gameState = 1;
 		prevFrameTime = performance.now()/1000;
 		window.requestAnimationFrame(update);
@@ -102,6 +112,10 @@ function update() {
 }
 
 function update_physics() {
+	entities["Player"].forEach(function(player) {
+		player.apply_physics();
+	})
+
 	/*
 	for each (var obstacle in sandbags) {
 
@@ -115,6 +129,12 @@ function update_physics() {
 function draw_frame() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 
+	for (var entityType in entities) {
+		entities[entityType].forEach(function(entity){
+			entity.draw();
+		});
+	};
+
 	//temporary border to see resizing
 	context.lineWidth = 10;
 	context.strokeStyle = "#ff00ff";
@@ -122,19 +142,18 @@ function draw_frame() {
 	context.stroke(); 
 }
 
-function draw_entity(entity) {
-
-}
-
 // Add rotation, transparency, and draw all objects from their centers
-// left edge @ x = -1
-// right edge @ x = 1
-// bottom edge @ y = -1
-// top edge @ y = 1
-// center @ x,y = 0,0av@
+// left edge @ x = 0
+// right edge @ x = GAME_DISP_WIDTH - 1
+// bottom edge @ y = GAME_DISP_HEIGHT - 1
+// top edge @ y = 0
 function draw_sprite(sprite, posX = 0, posY = 0, rot = 0, scale = 1, trans = 1) {
-	console.log("ran in here");
-	context.drawImage(spritesheet, sprite[0], sprite[1], sprite[2], sprite[3], posX, posY, sprite[2]*dispScale, sprite[3]*dispScale);
+	context.globalAlpha = trans;
+	context.rotate(rot);
+	context.drawImage(spritesheet, sprite[0], sprite[1], sprite[2], sprite[3],
+		posX*dispScale, posY*dispScale, sprite[2]*scale*dispScale, sprite[3]*scale*dispScale);
+	context.globalAlpha = 1;
+	context.rotate(-rot);
 }
 
 /*
@@ -148,19 +167,47 @@ function draw_sprite(sprite, dx = 0, dy = 0, scale = 1, transparency = 1) {
 
 
 class Entity {
-	constructor(width, height, rotation) {
-		this.height = height;
-		this.width = width;
-		this.rotation = rotation || 0.0;
+	constructor() {
+		this.posX = GAME_DISP_WIDTH / 2.0;
+		this.posY = GAME_DISP_HEIGHT / 2.0;
+		this.rot = 0;
+		this.scale = 1;
+		this.trans = 1;
+		this.sprite;
+		this.entityType;
+	}
+
+	// Draw each entity from it's center
+	draw() {
+		var drawWidth = this.sprite[2]*this.scale;
+		var drawHeight = this.sprite[3]*this.scale;
+		var adjX = this.posX - drawWidth / 2.0;
+		var adjY = this.posY - drawHeight / 2.0;
+		//debugger;
+		draw_sprite(this.sprite, adjX, adjY, this.rot, this.scale, this.trans);
 	}
 }
 
 class Player extends Entity {
-	constructor(width, height, rotation) {
-		super(width, height, rotation);
-		this.posX = 0;
-		this.posY = 0;
-		this.speed = 5;
+	constructor() {3
+		super();
+		this.entityType = "Player";
+		this.sprite = spritemap["player"][0];
+		this.state = 0;
+		this.speed = GAME_DISP_HEIGHT/2;
+	}
+
+	apply_physics() {
+		var motionX = keyStates[68] - keyStates[65]; // left - right
+		var motionY = keyStates[83] - keyStates[87]; // up - down
+		if(motionX && motionY) {	// euclidean geometry
+			motionX *= INV_ROOT_2;
+			motionY *= INV_ROOT_2;
+		}
+		if(motionX || motionY) this.state = 1;
+		else this.state = 0;
+		this.posX += motionX * this.speed * tpl;
+		this.posY += motionY * this.speed * tpl;
 	}
 
 	check_collision(entity) {
